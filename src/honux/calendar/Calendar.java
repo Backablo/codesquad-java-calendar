e93cf731391fc4a4c6e8a7fd6b0e23d8a61404e1
Kyu <Kyu@DESKTOP-MARC9UT>
2021-03-03 14:56:58 +0900

    Update calendar main()

--- src/honux/calendar/Calendar.java
+++ src/honux/calendar/Calendar.java
@@ -3,7 +3,10 @@ package honux.calendar;
 public class Calendar {
 
 	public static void main(String[] args) {
-		System.out.println("Hello, Celendar");
+		System.out.println("Hello Calendar");
+
+		
+		
 
 	}
 
