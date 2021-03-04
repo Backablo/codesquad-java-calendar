2aa614b8997bd3cfee97838ce46032496216634f
Kyu <Kyu@DESKTOP-MARC9UT>
2021-03-04 09:55:15 +0900

    repeat

--- src/honux/calendar/Calendar1.java
+++ src/honux/calendar/Calendar1.java
@@ -11,19 +11,24 @@ public class Calendar1 {
 	}
 
 	public static void main(String[] args) {
-		System.out.println("일  월   화    수   목   금    토");
-		System.out.println("-----------------------------");
-		System.out.println(" 1   2   3    4    5   6    7");
-		System.out.println(" 8   9  10   11   12  13   14");
-		System.out.println("15  16  17   18   19  20   21");
-		System.out.println("22  23  24   25   26  27   28");
+//		System.out.println("일  월   화    수   목   금    토");
+//		System.out.println("-----------------------------");
+//		System.out.println(" 1   2   3    4    5   6    7");
+//		System.out.println(" 8   9  10   11   12  13   14");
+//		System.out.println("15  16  17   18   19  20   21");
+//		System.out.println("22  23  24   25   26  27   28");
 
-		System.out.println("원하시는 달을 입력하세요.");
 		Scanner scanner = new Scanner(System.in);
 		Calendar1 cal = new Calendar1();
-		int Month = scanner.nextInt();
 
-		System.out.println(Month + "월의 마지막 날은 " + cal.getMaxDaysOfMonth(Month) + "입니다");
+		System.out.println("반복횟수를 입력하세요.");
+		int repeat = scanner.nextInt();
+		for (int i = 0; i < repeat; i++) {
+			System.out.println("원하시는 달을 입력하세요.");
+			int Month = scanner.nextInt();
+			System.out.println(Month + "월의 마지막 날은 " + cal.getMaxDaysOfMonth(Month) + "입니다");
+		}
+		System.out.println("Bye");
 
 		scanner.close();
 
